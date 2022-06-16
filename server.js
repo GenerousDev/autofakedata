7b50e19a402efcef29145514b677da67dab1186b
Abubakar Abdullahi <39764887+GenerousDev@users.noreply.github.com>
2022-06-16 20:04:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -75,4 +75,4 @@ app.get('/', (req, res) => {
   res.send(Generate.phoneNumber());
 });
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(process.env.PORT || 3000, () => console.log('Example app is listening on port 3000.'));
